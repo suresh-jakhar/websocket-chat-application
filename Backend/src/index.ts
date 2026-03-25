cf3178cb28638a21e64134d199e6d1df5df2e0da
Suresh Jakhar <gettingsuresh@gmail.com>
2026-03-25 20:01:40 +0530

    erase ping pong app

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,16 +1,2 @@
 import { WebSocketServer } from "ws";
 
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", function (socket) {
-  console.log("user connected");
-
-  socket.on("message", (e) => {
-    const message = e.toString();
-    console.log("Received:", message);
-
-    if (message === "ping") {
-      socket.send("pong");
-    }
-  });
-});
