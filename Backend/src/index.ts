9e34419d5b2060b489e091b7f4765d1048695b00
Suresh Jakhar <gettingsuresh@gmail.com>
2026-03-25 20:03:55 +0530

    initialize chat room

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,2 +1,12 @@
+import express from "express";
 import { WebSocketServer } from "ws";
 
+const app = express();
+const server = app.listen(3000);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", ws =>
+  ws.on("message", msg =>
+    wss.clients.forEach(c => c.send(msg.toString()))
+  )
+);
